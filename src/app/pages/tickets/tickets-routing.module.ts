82890d0047a8de8ff710d6cdb99907ff0dc425a2
Elizaveta Kritskaya <post.kri@gmail.com>
2023-03-19 23:03:22 +0300

    add tickets

--- src/app/pages/tickets/tickets-routing.module.ts
+++ src/app/pages/tickets/tickets-routing.module.ts
@@ -2,9 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AuthComponent} from "../auth/auth.component";
 import {TicketsComponent} from "./tickets.component";
+import {TicketListComponent} from "./ticket-list/ticket-list.component";
 
 const routes: Routes = [
-  { path: '', component: TicketsComponent },
+  { path: '', component: TicketsComponent,
+    children:[
+      {
+        path: 'tickets-list',
+        component: TicketListComponent
+      }
+    ]
+
+  },
 ];
 
 @NgModule({
