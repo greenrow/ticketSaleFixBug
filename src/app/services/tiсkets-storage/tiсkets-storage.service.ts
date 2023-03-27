3c514e4f587d733a357cb6681a05f18eaff975eb
Elizaveta Kritskaya <post.kri@gmail.com>
2023-03-27 19:49:07 +0300

    add tickets-storage

---
+++ src/app/services/tiсkets-storage/tiсkets-storage.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {ITour} from "../../models/tours";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TiсketsStorageService {
+  private ticketStorage: ITour[]
+
+  constructor() { }
+
+
+
+
+  setStorage(data: ITour[]): void {
+    this.ticketStorage = data;
+    // запись данных в this.ticketStorage
+  }
+  getStorage(): ITour[] {
+    return this.ticketStorage;
+    // возвращает в this.ticketStorage
+  }
+}
