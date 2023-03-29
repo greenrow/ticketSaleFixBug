3a75cce571f88f19384117c9190ed53c76fb8327
Elizaveta Kritskaya <post.kri@gmail.com>
2023-03-29 18:19:00 +0300

    change ticket-list

--- src/app/pages/tickets/ticket-list/ticket-list.component.ts
+++ src/app/pages/tickets/ticket-list/ticket-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {TicketService} from "../../../services/tickets/ticket.service";
 import {ITour} from "../../../models/tours";
+import { Router} from "@angular/router";
+import {TiсketsStorageService} from "../../../services/tiсkets-storage/tiсkets-storage.service";
+
 
 @Component({
   selector: 'app-ticket-list',
@@ -11,15 +14,21 @@ export class TicketListComponent implements OnInit {
   tickets: ITour[];
 
 
-  constructor(private ticketService: TicketService) { }
+  constructor(private ticketService: TicketService,
+              private router: Router,
+              private ticketStorage: TiсketsStorageService) { }
 
   //Подписаться на изменения, которые произойдут при запросе на сервер, параметром передается асинхронная операция (data)
   ngOnInit(): void {
     this.ticketService.getTickets().subscribe(
       (data) => {
         this.tickets = data;
+        this.ticketStorage.setStorage(data) // запрос на тур записывается
       }
     )
+  }
+  goToTicketInfoPage(item: ITour){
+    this.router.navigate([`/tickets/ticket/${item.id}`])
   }
 
 }
