0017c7ad59f4c92d95b0af36e6d5a8ef3e46c1bc
Elizaveta Kritskaya <post.kri@gmail.com>
2023-03-31 21:31:05 +0300

    change ticket-list

--- src/app/pages/tickets/ticket-list/ticket-list.component.ts
+++ src/app/pages/tickets/ticket-list/ticket-list.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {TicketService} from "../../../services/tickets/ticket.service";
 import {ITour} from "../../../models/tours";
 import { Router} from "@angular/router";
 import {TiсketsStorageService} from "../../../services/tiсkets-storage/tiсkets-storage.service";
+import {BlockStyleDirective} from "../../../directive/block-style.directive";
 
 
 @Component({
@@ -12,6 +13,10 @@ import {TiсketsStorageService} from "../../../services/tiсkets-storage/tiсket
 })
 export class TicketListComponent implements OnInit {
   tickets: ITour[];
+  ticketsCopy: ITour[];
+
+  @ViewChild('tourWrap', {read: BlockStyleDirective}) blockDirective: BlockStyleDirective;
+  @ViewChild('tourWrap') tourWrap: ElementRef;
 
 
   constructor(private ticketService: TicketService,
@@ -22,13 +27,36 @@ export class TicketListComponent implements OnInit {
   ngOnInit(): void {
     this.ticketService.getTickets().subscribe(
       (data) => {
-        this.tickets = data;
-        this.ticketStorage.setStorage(data) // запрос на тур записывается
+        this.tickets = data; //вставка нового шаблона
+        this.ticketsCopy = [...this.tickets] //содержит все данные
+        this.ticketStorage.setStorage(data); // запрос на тур записывается
+
       }
     )
   }
+
+  ngAfterViewInit(){
+
+  }
   goToTicketInfoPage(item: ITour){
     this.router.navigate([`/tickets/ticket/${item.id}`])
+  }
+
+  directiveRenderComplete(ev: boolean){
+    const el: HTMLElement = this.tourWrap.nativeElement;
+    el.setAttribute('style', 'background-color: #f5f5dc')
+    this.blockDirective.initStyle(0)
+  }
+// поиск туров
+  findTours(ev: Event):void{
+    const searchValue = (<HTMLElement>ev.target).nodeValue;
+
+    if(searchValue){
+      this.tickets = this.ticketsCopy.filter((el) => el.name.includes(searchValue));
+    }
+    else {
+      this.tickets = [...this.ticketsCopy]
+    }
   }
 
 }
