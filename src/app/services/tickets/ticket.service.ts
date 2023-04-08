507d83eb3896337e7a25e14faf27b94516ef3528
Elizaveta Kritskaya <post.kri@gmail.com>
2023-04-08 17:26:04 +0300

    change ticket

--- src/app/services/tickets/ticket.service.ts
+++ src/app/services/tickets/ticket.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import {TicketRestService} from "../rest/ticket-rest.service";
-import {Observable} from "rxjs";
+import {Observable, Subject} from "rxjs";
 import {ITour} from "../../models/tours"
+import {ITourTypeSelect} from "../../models/tours";
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,22 @@ export class TicketService {
 
   constructor(private ticketServiceRest: TicketRestService) { }
 
+  private ticketSubject = new Subject<ITourTypeSelect>()
+
 //Вызывает метод ticketServiceRest
   getTickets(): Observable<ITour[]>{
     return this.ticketServiceRest.getTickets()
   }
+
+  // 1 вариант доступа к Observable
+  readonly ticketType$ = this.ticketSubject.asObservable();
+
+  // 2 вариант доступа к Observable
+  // getTicketTypeObservable(): Observable<ITourTypeSelect> {
+  //   return this.ticketSubject.asObservable();
+  // }
+  //
+  // updateTour(type:ITourTypeSelect): void {
+  //   this.ticketSubject.next(type);
+  // }
 }
