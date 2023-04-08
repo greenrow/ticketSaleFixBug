fdd797aa8dff77a5fea51a28049d70cac6a10a2a
Elizaveta Kritskaya <post.kri@gmail.com>
2023-04-08 17:26:44 +0300

    change tours

--- src/app/models/tours.ts
+++ src/app/models/tours.ts
@@ -4,9 +4,12 @@ export interface ITour {
   tourOperator: string,
   price: string,
   img: string,
-  id: string
+  id: string,
+  type: string,
+  date: string
 }
 export interface ITourTypeSelect {
-  label: string,
-  value: string
+  label?: string,
+  value?: string,
+  date?: string
 }
